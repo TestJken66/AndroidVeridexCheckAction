2e3eaa18eaf18ce18630909f9f16a85f978cf0b0
sanbo.xyz <sanbo.xyz@gmail.com>
2021-10-25 17:16:51 +0800

    update

---
+++ dev-sdk/src/main/java/me/sdk/dev/Los.java
@@ -0,0 +1,18 @@
+package me.sdk.dev;
+
+import android.util.Log;
+
+/**
+ * @Copyright © 2021 sanbo Inc. All rights reserved.
+ * @Description: TODO
+ * @Version: 1.0
+ * @Create: 2021/10/25 5:12 下午
+ * @author: sanbo
+ */
+public class Los {
+    private static final String TAG = "Los";
+
+    public static void i(String s) {
+        Log.i(TAG, s);
+    }
+}
